01207d2d5bb53339955c1d47a9ab3845796ad125
Sobhan <sobhanimani81@gmail.com>
2023-01-27 17:56:57 +0330

    Added serializers

---
+++ store/api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from orders.models import Order, OrderItem
+from customers.models import Address
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['item', 'quantity']
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
+        exclude = ['profile', 'id', 'created_at', 'is_deleted']
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+    address = AddressSerializer(many=False)
+    class Meta:
+        model = Order
+        fields = ['price', 'address', 'status', 'items']
