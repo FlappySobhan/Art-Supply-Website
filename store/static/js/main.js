322645e976fd2388161b7f8789cb526de78db7f9
Sobhan <sobhanimani81@gmail.com>
2023-02-06 00:26:14 +0330

    Added front end js for APIs

--- store/static/js/main.js
+++ store/static/js/main.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 60) {
-        $('#toTopBtn').fadeIn();
-      } else {
-        $('#toTopBtn').fadeOut();
-      }
-    });
-  
-    $('#toTopBtn').click(function() {
-      $("html, body").animate({
-        scrollTop: 0
-      }, 500);
-      return false;
-    });
+$(document).ready(function () {  
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 60) {
+      $('#toTopBtn').fadeIn();
+    } else {
+      $('#toTopBtn').fadeOut();
+    }
+  });
+
+  $('#toTopBtn').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
 });
